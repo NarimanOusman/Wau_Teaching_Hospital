4d7df4d714cc24946ff7df08fadad97895f743bf
NarimanOusman <narimanrobo8@gmail.com>
2026-01-31 15:12:22 -0800

    feat: Add initial hospital website pages, views, and URL routing.

--- hospital/urls.py
+++ hospital/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('home/', views.home, name='home'),
+    path('appointment/', views.appointment, name='appointment'),
+    path('services/', views.services, name='services'),
+    path('doctors/', views.doctors, name='doctors'),
+    path('accident_report/', views.accident_report, name='accident_report'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
--- hospital/views.py
+++ hospital/views.py
@@ -6,3 +6,21 @@ def index(request):
 
 def home(request):
     return render(request, 'home.html')
+
+def appointment(request):
+    return render(request, 'appointment.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def doctors(request):
+    return render(request, 'doctors.html')
+
+def accident_report(request):
+    return render(request, 'accident_report.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
