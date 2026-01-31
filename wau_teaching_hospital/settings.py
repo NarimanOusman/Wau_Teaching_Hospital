04cca1e9d25543be158f626347e86c720dfe379f
NarimanOusman <narimanrobo8@gmail.com>
2026-01-31 15:22:15 -0800

    feat: Initialize Django project with base settings, `decouple`, `whitenoise`, and `hospital` app, along with `requirements.txt`.

--- wau_teaching_hospital/settings.py
+++ wau_teaching_hospital/settings.py
@@ -19,13 +19,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+import os
+from decouple import config
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dqe__5f67u7r9o8-ywh*uoi=yzy9a3-&84-ds-i*4xusq_izo='
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-dqe__5f67u7r9o8-ywh*uoi=yzy9a3-&84-ds-i*4xusq_izo=')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*'] # In production, replace with your actual Render URL
 
 
 # Application definition
@@ -42,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
